6f91d1bc5a20ae7f769756f9cc634e488cb8db09
programmingfire@gmail.com <squadguy@github.com>
2015-06-08 18:41:19 -0500

    push monday's inclass example

---
+++ monday_inclass_example/chris_wampnar_example1.cpp
@@ -0,0 +1,41 @@
+/* Christopher Wampnar: This is my first program
+Summer 2015 (8 week f2f seesion)*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int my_num;
+	my_num = 12 + 45/3 - 5 * 2 + 4 - 5 + 88;
+	cout << my_num;
+
+	return 0;
+}
+
+int main3(){
+	float my_gpa = 3.56; //Initialization
+
+	return 0;
+}
+
+int main2(){
+
+	int num_stu, num_cls; //declaration statement
+
+	//Move 15 into num_stu
+	//Left hand side is ALWAYS a variable
+	// = is the assignement operator (NOT THE EQUAL TO OP.)
+	//Right hand side is the value to be placed in LHS var
+	num_stu = 15; //assignement statement
+
+	cout << "num_stu" << num_stu << endl;
+	return 0;
+}
+
+int main1() {
+	cout << 'C' << 'h' << 'r' << 'i' <<'s' << ' ' << "Wampnar" << endl;
+	cin.get();
+	return 0;
+
+}
